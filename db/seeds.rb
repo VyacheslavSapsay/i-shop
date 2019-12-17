1cfbc43045a1e352e841cb983f886ceae36600a1
SsPay <truesspay@gmail.com>
2019-12-17 20:00:25 +0200

    styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,13 @@ Category.create(title: 'Magazine', description: "The best and most diverse magaz
 Category.create(title: 'Manga', description: "The best and most diverse manga")
 
 10.times do |n|
-  Product.create(title: "Book#{n}", description: "Book#{n} is a very interesting book", category_id: 1)
+  Product.create(title: "Book#{n}", description: "Book#{n} is a very interesting book", price: rand(1..200), category_id: 1)
 end
 
 10.times do |n|
-  Product.create(title: "Magazine#{n}", description: "Magazine#{n} is a very interesting magazine", category_id: 2)
+  Product.create(title: "Magazine#{n}", description: "Magazine#{n} is a very interesting magazine", price: rand(1..200), category_id: 2)
 end
 
 10.times do |n|
-  Product.create(title: "Manga#{n}", description: "Manga#{n} is a very interesting manga", category_id: 3)
+  Product.create(title: "Manga#{n}", description: "Manga#{n} is a very interesting manga", price: rand(1..200), category_id: 3)
 end
