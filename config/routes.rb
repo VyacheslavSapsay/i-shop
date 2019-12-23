0154a43ccedfc768f2ff40167b0af2f879e5104b
SsPay <truesspay@gmail.com>
2019-12-23 16:02:24 +0200

    add categories views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'products#index'
   resources :products
+  resources :categories
 
   scope '/admin' do
     resources :categories, controller: 'backoffice/categories', as: "admin_categories"
