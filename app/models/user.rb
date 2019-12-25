49701856ad767df22fc4b918174734b8fcd4d9de
SsPay <truesspay@gmail.com>
2019-12-25 21:06:31 +0200

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  validate :pass_val
+  validates :password, presence: true,
+                      length: { minimum: 8 }
   attr_accessor :password, :password_confirmation
+
+
+  def pass_val
+    errors.add(:password, 'must contain 1 small letter, 1 capital letter and minimum 8 symbols') if password.count('a-z') <= 0 || password.count('A-Z') <= 0
+  end
 end
