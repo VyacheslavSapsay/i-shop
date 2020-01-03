1f19064ac418c97cd6271a5e6c5e83d791bb8a8c
SsPay <truesspay@gmail.com>
2020-01-03 15:42:21 +0200

    add trix

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,5 @@ class Product < ApplicationRecord
   validates :title, uniqueness: true, presence: true
   validates :description, presence: true
   belongs_to :category
-
+  has_rich_text :description
 end
