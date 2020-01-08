ba882df81f0f9aca7e1f60a788b54972fab280ff
SsPay <truesspay@gmail.com>
2020-01-08 14:21:50 +0200

    Fix seeds

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   mount_uploaders :images, ProductsImageUploader
   validates :title, uniqueness: true, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
+  validates :category_ids, presence: true
   has_many :product_categories
   has_many :categories, through: :product_categories
   has_rich_text :description
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Category.create(title: 'Book', description: "The best and most diverse books")
-Category.create(title: 'Magazine', description: "The best and most diverse magazines")
+Category.create(title: 'Book', description: "The best and most diverse books", id: 1)
+Category.create(title: 'Magazine', description: "The best and most diverse magazines", id: 2)
 #
 #
-Category.create(title: 'Manga', description: "The best and most diverse manga")
+Category.create(title: 'Manga', description: "The best and most diverse manga", id: 3)
 #
 10.times do |n|
-  Product.create(title: "Book#{n}", description: "Book#{n} is a very interesting book", price: rand(1..200), category_id: 1)
+  Product.create(title: "Book#{n}", description: "Book#{n} is a very interesting book", price: rand(1..200), category_ids: ["1"])
 end
 #
 10.times do |n|
-  Product.create(title: "Magazine#{n}", description: "Magazine#{n} is a very interesting magazine", price: rand(1..200), category_id: 2)
+  Product.create(title: "Magazine#{n}", description: "Magazine#{n} is a very interesting magazine", price: rand(1..200), category_ids: ["2"])
 end
 #
 10.times do |n|
-  Product.create(title: "Manga#{n}", description: "Manga#{n} is a very interesting manga", price: rand(1..200), category_id: 3)
+  Product.create(title: "Manga#{n}", description: "Manga#{n} is a very interesting manga", price: rand(1..200), category_ids: ["3"])
 end
 #
 User.create([email: 'admin@example.com', password: 'AdminPassword', admin: true])
