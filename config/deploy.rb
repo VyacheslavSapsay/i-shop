ba30ae9b503f585411916f0e837f7e689bfecb09
SsPay <truesspay@gmail.com>
2020-01-22 20:54:02 +0200

    deploy fix??

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :rvm_ruby_version, '2.6.3'
 
 set :linked_files, %w{config/secrets.yml}
 
-append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/puma.rb', 'config/master.key'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/puma.rb'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
 
 set :config_example_suffix, '.example'
