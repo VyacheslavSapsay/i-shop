9b091aa98d9b14810d28163ee032f1d23303c108
SsPay <truesspay@gmail.com>
2020-01-22 22:27:19 +0200

    vsego lish eshe odin commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,9 +11,9 @@ set :pty, true
 
 set :rvm_ruby_version, '2.6.3'
 
-set :linked_files, %w{config/secrets.yml}
+set :linked_files, %w{config/master.key config/secrets.yml}
 
-append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/puma.rb'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/puma.rb', 'config/master.key'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
 
 set :config_example_suffix, '.example'
